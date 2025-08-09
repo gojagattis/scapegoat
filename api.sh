2d1b577da92df28495713c2d1390d992d73c4b45
Raoul Ghosh <raoul.ghosh@orryworx.com>
2025-08-09 13:44:26 +0530

    Fix script issues

--- api.sh
+++ api.sh
@@ -3,13 +3,14 @@
 ignore=(litestream_lock litestream_seq holding_area)
 
 if [ ! -d "prisma/migrations" ] ; then
+  rm .env
   mv prisma orm
   npm install prisma @prisma/client bcrypt jsonwebtoken accesscontrol winston dayjs nodemailer node-cache pluralize
   npx prisma init --datasource-provider sqlite
   cp -f orm/* prisma
   rm -rf orm
 
-  if [ ! grep -Fxq package.json "\"prisma\": {" ]; then
+  if ! grep -Fxq '	"prisma": {' package.json; then
     sed -i '/.*devDependencies.*/i \\t"prisma": {\n\t\t"seed": "node prisma/seed.js"\n\t},' package.json
   fi
 
@@ -38,12 +39,13 @@ if [ ! -d "prisma/migrations" ] ; then
   echo "VITE_EXTENDED_SESSION=\"1y\"" >> .env
   echo "" >> .env
 
-  sed -i '/^.env$/d' .gitignore
-  echo "server.log" >> .gitignore
-  echo "/prisma/dev.db*" >> .gitignore
-  echo "/prisma/migrations" >> .gitignore
-  echo "/.idea" >> .gitignore
-
+  if ! grep -Fxq '/prisma/migrations' package.json; then
+    sed -i '/^.env$/d' .gitignore
+    echo "server.log" >> .gitignore
+    echo "/prisma/dev.db*" >> .gitignore
+    echo "/prisma/migrations" >> .gitignore
+    echo "/.idea" >> .gitignore
+  fi
   git init && git checkout -b main && git add -A && git commit -m "Init"
 
 else
